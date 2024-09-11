5aa4251cd8d1542967e7d3ca167894a97dc59609
LouisePrd <louise.peridy22@gmail.com>
2024-09-11 20:30:31 +0200

    change colors + import components

--- personnal-notebook/src/App.vue
+++ personnal-notebook/src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Poem from './components/Poem.vue'
+import Text from './components/Text.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <link rel="stylesheet" href="./assets/main.css">
+    <Header />
   </header>
 
   <main>
-    <TheWelcome />
+
   </main>
+
+  <footer>
+    <Footer />
+  </footer>
+
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
