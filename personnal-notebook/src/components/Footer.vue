de787bd85d6c04192a1e74b136efb4ea1f2fb78c
LouisePrd <louise.peridy22@gmail.com>
2024-09-11 20:30:08 +0200

    Header and Footer : basics

---
+++ personnal-notebook/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="footer">
+        <p>© 2024 Personal Notebook - Lwiz</p>
+    </div>
+
+</template>
+
+<style scoped>
+.footer {
+    color: rgb(0, 0, 0);
+    text-align: center;
+    padding: 1rem;
+}
+
+.footer p {
+    margin: 0;
+}
+
+</style>
---
+++ personnal-notebook/src/components/Header.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="navbar">
+        <div class="navLogo">
+            <img src="" alt="logo" />
+        </div>
+        <div class="navMenu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">French</a></li>
+                <li><a href="#">English</a></li>
+                <li><a href="#">All</a></li>
+            </ul>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    color: rgb(255, 255, 255);
+}
+
+.navLogo img {
+    width: 50px;
+    height: 50px;
+}
+
+.navMenu ul {
+    display: flex;
+    list-style: none;
+}
+
+.navMenu ul li {
+    margin-right: 1rem;
+}
+
+.navMenu a {
+    text-decoration: none;
+    color: rgb(0, 0, 0);
+}
+
+.navMenu a:hover {
+    text-decoration: underline;
+}
+
+
+
+</style>
